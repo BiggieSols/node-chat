d846ee882ae9f46fd6ca245ed5feaa77cf88d980
enquea <student@appacademy.io>
2014-01-13 17:19:03 -0800

    Multi room supported

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -7,11 +7,31 @@
 
   Chat.prototype.sendMessage = function(message) {
     msgArr = message.split(" ");
-    if(msgArr[1] == "/nick") {
-      console.log("attempting to update nickname");
-      this.socket.emit('nicknameChangeRequest', msgArr[2]);
+    if(msgArr[1].slice(0,1) === "/") {
+      this.performAction(msgArr)
     } else {
       this.socket.emit('message', message);
     }
+    // if(msgArr[1] == "/nick") {
+    //   console.log("attempting to update nickname");
+    //   this.socket.emit('nicknameChangeRequest', msgArr[2]);
   }
-})(this)
+
+  Chat.prototype.performAction = function(data) {
+    var action = data[1];
+    var param = data[2];
+
+    switch(action) {
+    case "/nick":
+      console.log("attempting to update nickname");
+      this.socket.emit('nicknameChangeRequest', param);
+      break;
+    case "/join":
+      console.log("attempting to change room");
+      this.socket.emit('joinRoom', param);
+      break;
+    default:
+      this.socket.emit('errorMessage', "Not a valid command");
+    }
+  }
+})(this);
